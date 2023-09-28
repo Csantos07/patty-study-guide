f4a7f64f6db81b63959dc53278763a3e938970a9
Carlo Santos <rcarlosantos89@gmail.com>
2023-09-28 13:16:47 -0500

    on the fly with patty

--- maze.js
+++ maze.js
@@ -16,6 +16,45 @@ let maze = [
 
 // For the above maze, a possible exit path can be RRDDLLDDRRRRRR.
 // Write a function that accepts a two - dimensional array for the maze and an array with two values for the current position in the maze.For example, you may call the function like this: mazeSolver(maze, [0, 0]) to indicate that the starting position is at the top - left corner.
-function mazeSolver() {
 
+
+//Check which direction can we go from here
+// Refactor later
+function mazeSolver(maze, index = [0, 0]) {
+  let row = index[0];
+  let column = index[1];
+  let currentStepInQuestion = "";
+  let currentRow = maze[row];
+  let rightBound = currentRow.length;
+
+  let arrayOfSteps = [];
+
+  //right first
+  // ------------------------------------------
+  // currentStepInQuestion = maze[row][column + 1]
+  for (let i = 0; i < row.length; i++) {
+    
+  }
+
+
+  if (column + 1 >= rightBound) {
+    console.log("we can't continue")
+  } else {
+    if (currentStepInQuestion === " ") {
+      console.log("Yayyyy");
+      arrayOfSteps.push("Right");
+      column += 1;
+    } else if (currentStepInQuestion === "*") {
+      console.log("Can't Go Here :( ");
+    }
+  }
+
+
+
+
+  // down
+  // left
+  // up
 }
+
+mazeSolver(maze);
